d7820645ecba92f3876a514e9f0f387d407a68ba
nikitha <nikki@nikithas-MacBook-Air.local>
2025-02-17 19:11:12 -0700

    Initial commit - Spring Boot project setup for Mars Visit Application

---
+++ src/main/java/com/marsvisit/mars_visit_application/MarsVisitApplication.java
@@ -0,0 +1,13 @@
+package com.marsvisit.mars_visit_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsVisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsVisitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marsvisit/mars_visit_application/MarsVisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsvisit.mars_visit_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsVisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
